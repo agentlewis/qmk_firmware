e987ee671099c19f98a45c36a9b9a9b04a508f44
Dan <dan.lewis@bamboocreative.nz>
2022-10-03 14:47:44 +0200

    init agentlewis layout

---
+++ keyboards/omkbd/ergodash/mini/keymaps/agentlewis/keymap.c
@@ -0,0 +1,113 @@
+/* Copyright 2020 Toyoshima Hidenori <t@toyo.dev>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+};
+
+enum custom_keycodes {
+    QWERTY = SAFE_RANGE,
+    LOWER,
+    RAISE,
+};
+
+#define EISU LALT(KC_GRV)
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Qwerty
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |  ESC |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |  TAB |   A  |   S  |   D  |   F  |   G  |   [  |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   -  |                    |   =  |   N  |   M  |   ,  |   .  |   /  |PageUp|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  Alt |   `  | RAISE||||||||  GUI | Raise| Space|||||||| Enter| Space|  Gui |||||||| Lower|   @  |   #  |PageDN|
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+    [_QWERTY] = LAYOUT(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LCBR,                        KC_RBRC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LCBR,                        KC_RBRC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS,                        KC_EQL,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PGUP,
+    KC_LCTL, KC_LALT, KC_GRV, RAISE,            KC_LGUI, KC_BSPC, KC_DEL,        KC_ENT, KC_SPC,   KC_RGUI,          LOWER,   KC_AT,   KC_HASH, KC_PGDN
+  ),
+
+  /* Lower
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |  ESC |   1  |   2  |   3  |   4  |   5  |   _  |                    |   +  | 6    | 7    |  8   | 9    |   0  |   |  |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Ctrl |   ^  |   &  |   *  |   (  |   )  |   <  |                    |   >  | End  | Left | Down |  Up  | Right|  End |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   !  |   @  |   #  |   $  |   %  |   (  |                    |   )  |   ?  |   =  |   <  |   >  |   ?  |PageUp|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  Alt |   ~  | RAISE||||||||  GUI | Raise| Space|||||||| Enter| Space|  Gui |||||||| Lower|   @  |   #  |PageDN|
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  [_LOWER] = LAYOUT(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_UNDS,                        KC_PLUS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE,
+    KC_TAB,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LT,                          KC_GT,    KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,
+    KC_LSFT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LPRN,                        KC_RPRN,  KC_QUES, KC_EQL,  KC_LT,   KC_GT,   KC_QUES, KC_PGUP,
+    KC_LCTL, KC_LALT, KC_TILD, RAISE,            KC_LGUI, KC_BSPC, KC_DEL,        KC_ENT, KC_SPC,   KC_RGUI,          LOWER,   KC_AT,   KC_HASH, KC_PGDN
+  ),
+
+  /* Raise
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |  ESC |   1  |   2  |   3  |   4  |   5  |   _  |                    |   +  | 6    | 7    |  8   | 9    |   0  |   /  |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Ctrl |   ^  |   &  |   *  |   (  |   )  |   -  |                    |   +  | End  | Left | Down |  Up  | Right|  End |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   !  |   @  |   #  |   $  |   %  |   {  |                    |   }  |   _  |   +  |   <  |   >  |   ?  |PageUp|
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  Alt |  ~   | RAISE||||||||  GUI | Raise| Space|||||||| Enter| Space|  Gui |||||||| Lower|   @  |   #  |PageDN|
+   * ,----------------------------------------------------------------------------------------------------------------------.
+  */
+  [_RAISE] = LAYOUT(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_UNDS,                        KC_PLUS,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SLSH,
+    KC_TAB,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LT,                          KC_GT,    KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,
+    KC_LSFT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LPRN,                        KC_RPRN,  KC_QUES, KC_PLUS, KC_LT,   KC_GT,   KC_QUES, KC_PGUP,
+    KC_LCTL, KC_LALT, KC_TILD, RAISE,            KC_LGUI, KC_BSPC, KC_DEL,        KC_ENT, KC_SPC,   KC_RGUI,          LOWER,   KC_AT,   KC_HASH, KC_PGDN
+  )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
